729b5afed695f08957250fc9722cf74b6a8e63af
Dmitriy Kondak <dmitriykondak@gmail.com>
2020-09-16 00:09:57 +0300

    moved exception handling from StartClient class to Client class, added debugMod

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -2,83 +2,83 @@ import java.io.*;
 import java.net.Socket;
 
 public class Client implements IClient {
-    private static boolean socketListening = true;
-    private static String authorizationTeg = "a";
-    private static String loginTeg = "l";
-    private static String messageTeg = "m";
+    private static final String authorizationTag = "a";
+    private static final String loginTag = "l";
+    private static final String messageTag = "m";
 
+    private final boolean debugModEnable;
     private Socket clientSocket;
-    private BufferedReader reader;
+    private final BufferedReader reader;
     private BufferedReader in;
     private BufferedWriter out;
 
-    public Client(Socket clientSocket, BufferedReader reader, BufferedReader in, BufferedWriter out) {
-        this.clientSocket = clientSocket;
-        this.reader = reader;
-        this.in = in;
-        this.out = out;
-    }
+    public Client(String HOST, int PORT, boolean debugModEnable) {
+        this.debugModEnable = debugModEnable;
+        reader = new BufferedReader(new InputStreamReader(System.in));
 
-    @Override
-    public void authorization() throws IOException {
-        System.out.println("Enter your login");
-        String authorization = reader.readLine();
-
-        System.out.println(authorization);
-        authorization = Wrapper.wrap(authorizationTeg, Wrapper.wrap(loginTeg, authorization));
-
-        out.write(authorization + "\n");
-        out.flush();
-        String answer = in.readLine();
-        answer = Parser.parse(authorizationTeg, answer);
-        System.out.println(answer);
-        if (answer.equalsIgnoreCase("ok")) {
-            this.messaging();
-        } else {
-            this.authorization();
+        try {
+            clientSocket = new Socket(HOST, PORT);
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        } catch (IOException e) {
+            if (debugModEnable)
+                System.err.println(e);
         }
     }
 
     @Override
-    public void messaging() throws IOException {
-        SocketListener socketListener = new SocketListener();
+    public void authorization() {
+        try {
+            System.out.println("Enter your login");
+            String authorization = reader.readLine();
+
+            System.out.println(authorization);
+            authorization = Wrapper.wrap(authorizationTag, Wrapper.wrap(loginTag, authorization));
+
+            out.write(authorization + "\n");
+            out.flush();
+            String answer = in.readLine();
+            answer = Parser.parse(authorizationTag, answer);
+            System.out.println(answer);
+            if (answer.equalsIgnoreCase("ok")) {
+                this.messaging();
+            } else {
+                this.authorization();
+            }
+        } catch (IOException e) {
+            if (debugModEnable)
+                System.err.println(e);
+        }
+    }
+
+    @Override
+    public void messaging() {
+
+        SocketListener socketListener = new SocketListener(clientSocket,
+                in,
+                messageTag,
+                debugModEnable);
         socketListener.setDaemon(true);
         socketListener.start();
+
         while (clientSocket.isConnected()) {
-            System.out.println("Write message");
-            String word = reader.readLine();
-            if (word.equalsIgnoreCase("exit")) {
-                //socketListening = false;
-                socketListener.interrupt();
-                //in.close();
-                //out.close();
-                //reader.close();
-                return;
-            }
-            word = Wrapper.wrap(messageTeg, word);
+            try {
+                System.out.println("Write message");
+                String word;
 
-            out.write(word + "\n");
-            out.flush();
-        }
-    }
+                word = reader.readLine();
 
-    class SocketListener extends Thread {
-        @Override
-        public void run() {
-            String serverWord = null;
-
-            while (!clientSocket.isClosed()) {
-
-                try {
-                    
-                    serverWord = in.readLine();
-                    if (serverWord != null)
-                        serverWord = Parser.parse(messageTeg, serverWord);
-                    System.out.println(serverWord);
-
-                } catch (IOException e) {
-                    System.err.println(e);
+                if (word.equalsIgnoreCase("exit")) {
+                    socketListener.interrupt();
+                    return;
                 }
+                word = Wrapper.wrap(messageTag, word);
+
+                out.write(word + "\n");
+                out.flush();
+            } catch (IOException e) {
+                if (debugModEnable)
+                    System.err.println(e);
             }
         }
     }
--- Client/src/StartClient.java
+++ Client/src/StartClient.java
@@ -1,29 +1,13 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.Socket;
-
 public class StartClient {
 
     private static final String HOST = "localhost";
     private static final int PORT = 4004;
 
     public static void main(String[] args) {
-        try {
-            try (Socket clientSocket = new Socket(HOST, PORT);
-                 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                 BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-            ) {
-                Client client = new Client(clientSocket, reader, in, out);
-                client.messaging();
-            } finally {
-                System.out.println("Client was closed...");
-            }
-        } catch (IOException e) {
-            System.err.println(e);
-        }
+        boolean debugModEnable = true;
+
+        Client client = new Client(HOST, PORT, debugModEnable);
+        client.messaging();
+        System.out.println("Client was closed...");
     }
 }
