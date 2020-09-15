384eaccf0349894ca3a65ea4e65f6fc15ec26f17
Dmitriy Kondak <dmitriykondak@gmail.com>
2020-09-16 00:10:25 +0300

    SocketListener class moved to a separate

---
+++ Client/src/SocketListener.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+
+public class SocketListener extends Thread {
+    private final Socket clientSocket;
+    private final BufferedReader in;
+    private final String messageTag;
+    private final boolean debugModEnable;
+
+    public SocketListener(Socket clientSocket, BufferedReader in, String messageTag, boolean debugModEnable) {
+        this.clientSocket = clientSocket;
+        this.in = in;
+        this.messageTag = messageTag;
+        this.debugModEnable = debugModEnable;
+    }
+
+    @Override
+    public void run() {
+        String serverWord;
+        while (!clientSocket.isClosed()) {
+
+            try {
+                serverWord = in.readLine();
+                if (serverWord != null)
+                    serverWord = Parser.parse(messageTag, serverWord);
+                System.out.println(serverWord);
+
+            } catch (IOException e) {
+                if (debugModEnable)
+                    System.err.println(e);
+
+            }
+        }
+    }
+}
